875c958687280fa3592a2d219a8d05394bdf572f
Micah <micahshallom@gmail.com>
2023-10-07 08:05:46 +0100

    completed getMovies functionality; added some stylesheet css

--- static/script.js
+++ static/script.js
@@ -4,13 +4,15 @@ var movieForm =  document.getElementById("movie-form")
 submitBTN.addEventListener("click", submitMovies)
 listBTN.addEventListener("click", listMovies);
 
+const address = "http://localhost:8000"
+
 function listMovies() {
-    getMovies()
+    getMovies(address+"/movies")
 };
 
 function submitMovies(event) {
     event.preventDefault()
-    addMovie()
+    addMovie(address+"/movies")
 };
 // Add an event listener to the form's submit event To prevent the default form submission behavior .
 movieForm.addEventListener("submit", (event) => {
@@ -20,15 +22,19 @@ movieForm.addEventListener("submit", (event) => {
     }
 })
 
-async function addMovie() {
-    var url = "http://localhost:8000/movies"
+async function checkURLStatus(url) {
+    const response = await fetch(url);
 
+    if (!response.ok) {
+        throw new Error(`Network response was not okay: ${response.statusText}`)
+    };
+
+    return response;
+}
+
+async function addMovie(url) {
     try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`Network response was not okay: ${response.statusText}`)
-        }
+        const response = checkURLStatus(url)
 
         //  const formData = new FormData(movieForm)
         //  formData.forEach((val, key) => {
@@ -36,14 +42,14 @@ async function addMovie() {
         //  })
         //  console.log("FormData", formData)
 
-        var id = document.getElementById("id").value;
+        // var id = document.getElementById("id").value;
         var isbn = document.getElementById("isbn").value;
         var title = document.getElementById("title").value;
         var firstname = document.getElementById("dir-firstname").value;
         var lastname = document.getElementById("dir-lastname").value;
 
         var formData = {
-            id,
+            // id,
             isbn,
             title,
             director: {
@@ -54,10 +60,10 @@ async function addMovie() {
         
         await fetch(url, {
             method: "POST",
-            body: formData
+            body: JSON.stringify(formData),
         })
 
-        console.log("Success sending request")
+        console.log("Success sending request", response)
         
     } catch (error) {
         console.error(`Error fetching data: ${error}`)
@@ -66,6 +72,38 @@ async function addMovie() {
     
 };
 
-function getMovies() {
-    
+async function getMovies(url) {
+    try {
+
+       const response = await fetch(url,{
+        method: "GET"
+       })
+
+       if (!response.ok){
+        throw new Error(`Network response was not okay: ${response.statusText}`)
+       }
+
+       const responseBody = await response.json()
+       var container = document.querySelector(".movie-container")
+       container.innerHTML = ""
+
+       responseBody.forEach((item) => {
+           const itemContainer = document.createElement("div")
+
+
+        const itemHTML = `
+        <li class="title" >Title: ${item.title}</li>
+        <ul class="other-info">
+            <li>ID: ${item.id}</li>
+            <li>ISBN: ${item.isbn}</li>
+            <li>Director: ${item.director.firstname} ${item.director.lastname}</li>
+        </ul>
+        `;
+        itemContainer.innerHTML = itemHTML
+        container.appendChild(itemContainer)
+       })
+        
+    } catch (error) {
+        console.error(`Error fetching data ${error}`)
+    }
 };
