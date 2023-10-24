09a38d7def7fe698083b323659c101186d0e710c
Allison Durham <AdjectiveAllison@gmail.com>
2023-10-24 14:16:38 -0500

    Printing to console on controller actions

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -5,103 +5,117 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
 	"github.com/Micah-Shallom/pkg/models"
 	"github.com/Micah-Shallom/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
-func GetMovies(w http.ResponseWriter, r *http.Request){
+func GetMovies(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Action: GetMovies")
 	w.Header().Set("Content-Type", "application/json")
 	movies := models.GetAllMovies()
 	body, err := json.Marshal(movies)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error marshaling movies:", err)
+		return
 	}
+	fmt.Println("Successfully got all movies")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
 
-func GetMovie(w http.ResponseWriter, r *http.Request){
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Action: GetMovie")
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	ID := params["id"]
-	movieID, err := strconv.Atoi(ID); 
+	movieID, err := strconv.Atoi(ID)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error parsing ID:", err)
+		return
 	}
 	movie, _ := models.GetMovieByID(movieID)
-	body , jsonerr := json.Marshal(movie)
-	if jsonerr != nil {
-		fmt.Println(err)
+	body, err := json.Marshal(movie)
+	if err != nil {
+		fmt.Println("Error marshaling movie:", err)
+		return
 	}
+	fmt.Println("Successfully got movie by ID")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
 
-func CreateMovie(w http.ResponseWriter, r *http.Request){
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Action: CreateMovie")
 	w.Header().Set("Content-Type", "application/json")
 	CreateMovie := &models.Movie{}
 	utils.ParseBody(r, CreateMovie)
 	movie := CreateMovie.CreateMovie()
 	body, err := json.Marshal(movie)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error marshaling new movie:", err)
+		return
 	}
+	fmt.Println("Successfully created new movie")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
 
-func UpdateMovie(w http.ResponseWriter, r *http.Request){
-	newmovie := &models.Movie{}
+func UpdateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Action: UpdateMovie")
 	w.Header().Set("Content-Type", "application/json")
-	utils.ParseBody(r, newmovie)
+	newMovie := &models.Movie{}
+	utils.ParseBody(r, newMovie)
 	params := mux.Vars(r)
 	id := params["id"]
 	movieID, err := strconv.Atoi(id)
 	if err != nil {
-		fmt.Println("Error while parsing", err)
+		fmt.Println("Error parsing ID:", err)
+		return
 	}
 	movie, db := models.GetMovieByID(movieID)
-	if newmovie.Isbn != "" {
-		movie.Isbn = newmovie.Isbn
+
+	if newMovie.Isbn != "" {
+		movie.Isbn = newMovie.Isbn
 	}
-	
-	if newmovie.Title != "" {
-		movie.Title = newmovie.Title
+	if newMovie.Title != "" {
+		movie.Title = newMovie.Title
 	}
-	
-	if newmovie.Director.FirstName != "" {
-		movie.Director.FirstName = newmovie.Director.FirstName
+	if newMovie.Director.FirstName != "" {
+		movie.Director.FirstName = newMovie.Director.FirstName
 	}
-	
-	if newmovie.Director.LastName != "" {
-		movie.Director.LastName = newmovie.Director.LastName
+	if newMovie.Director.LastName != "" {
+		movie.Director.LastName = newMovie.Director.LastName
 	}
 	db.Save(&movie)
-	body, jsonerr := json.Marshal(movie)
-
-	if jsonerr != nil {
-		fmt.Println(jsonerr)
+	body, err := json.Marshal(movie)
+	if err != nil {
+		fmt.Println("Error marshaling updated movie:", err)
+		return
 	}
-
+	fmt.Println("Successfully updated movie")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
 
-func DeleteMovie(w http.ResponseWriter, r *http.Request){
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Action: DeleteMovie")
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	id := params["id"]
 	movieID, err := strconv.Atoi(id)
 	if err != nil {
-		fmt.Println("Error whle parsing", err)
+		fmt.Println("Error parsing ID:", err)
+		return
 	}
 	movie, _ := models.DeleteMovie(movieID)
-	body, jsonerr := json.Marshal(movie)
-	fmt.Println(string(body))
-	if jsonerr != nil {
-		fmt.Println(jsonerr)
+	body, err := json.Marshal(movie)
+	if err != nil {
+		fmt.Println("Error marshaling deleted movie:", err)
+		return
 	}
+	fmt.Println("Successfully deleted movie")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
